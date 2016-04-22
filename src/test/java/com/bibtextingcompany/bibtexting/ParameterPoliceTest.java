3a0bf803093279788995113fd06a0075de8f6496
Tuomas H <tuho@cs.helsinki.fi>
2016-04-22 13:22:05 +0300

    Testi ParameterPolicelle

---
+++ src/test/java/com/bibtextingcompany/bibtexting/ParameterPoliceTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bibtextingcompany.bibtexting;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+ 
+ public class ParameterPoliceTest {
+    
+    public ParameterPoliceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    /**
+     * Test of Validate method, of class StringValidator.
+     */
+    @Test
+    public void testEmpties() {
+        String[] params = new String[24];
+        
+        for (int i = 0; i<24; i++) {
+          params[i]="";
+        }
+        
+        params = ParameterPolice.process(params);
+        
+        boolean allErrors = true;
+        
+        for (int i = 0; i<24; i++) {
+          if (!(params[i].contains("<") && params[i].contains(">"))) {
+              allErrors=false
+          }
+        }
+        
+        
+        assertEquals(allErrors, true);
+
+        params = ParameterPolice.clean(params);
+        
+        boolean allNulls = true;
+        
+        for (int i = 0; i<24; i++) {
+          if (params[i]!=null) {
+              allNulls=false
+          }
+        }
+        
+             assertEquals(allNulls, true);
+
+    }
+    
+}
